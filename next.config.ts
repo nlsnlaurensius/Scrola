7c564ee04cad2be7cb8e7640d8e58c7c88519aa8
Nelson <nelsonlaurensius1234@gmail.com>
2026-02-16 16:48:47 +0700

    setup and backend

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['localhost:3000', 'localhost:3002'],
+    },
+  },
 };
 
 export default nextConfig;
